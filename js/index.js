ed21513bfb18f9eb17be80a217db2fc0b0568585
suruixin <507811581@qq.com>
2017-10-01 17:39:43 +0800

    3
    3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
+//组件
+Vue.component('banner',{
+	template:'<div v-on:wheel="wheel"></div><div class="shade"><div></div></div>',
+	created:function(){//生成前
+	},
+	mounted:function(){//生成后
+		$(this.$el).height($(this.$el).parent().height()*3)
+	},
+	methods:{
+		wheel:function(e){
+			if(e.wheelDelta){
+				var y = e.wheelDelta
+			}else{
+				var y = e.deltaY
+			}
+			switch(y){
+				case 120:
+				case -3:
+					console.log('top')
+				break;
+				case -120:
+				case 3:
+				break
+			}
+		}
+	}
+})
+//组件end
 
-var a = new Vue({
+var vue = new Vue({
 	el:'#home',
 	data:{
 		name:'aaa'
+	},
+	created:function(i,e){
+		$('#home').height($(document).height());
 	}
 })
