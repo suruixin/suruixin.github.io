e0706bb34e20f354b98e4ab29e202bbf9c04963e
suruixin <507811581@qq.com>
2017-10-18 17:51:07 +0800

    2
    2

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 //组件
 Vue.component('banner',{
-	template:'<div v-on:wheel="wheel"></div><div class="shade"><div></div></div>',
+	template:'<div v-on:wheel="wheel" @mousemove="hover" class="banner">'
+	+'<ul></ul>'
+	+'<ul></ul>'
+	+'<ul></ul>'
+	+'</div>',
 	created:function(){//生成前
 	},
 	mounted:function(){//生成后
-		$(this.$el).height($(this.$el).parent().height()*3)
+		$(this.$el).height($(this.$el).parent().height()*3);
 	},
 	methods:{
 		wheel:function(e){
+			$(this.$el).children().each(function(){
+				
+			})
 			if(e.wheelDelta){
 				var y = e.wheelDelta
 			}else{
@@ -22,17 +29,37 @@ Vue.component('banner',{
 				case 3:
 				break
 			}
+		},
+		hover:function(e){
+			var x = 
 		}
 	}
 })
 //组件end
-
 var vue = new Vue({
 	el:'#home',
 	data:{
-		name:'aaa'
+		name:'aaa',
+		scroll:''
+	},
+	methods:{
+		menu:function(){
+			this.scroll = $('body').scrollTop();
+        	console.log(this.scroll)
+		}
 	},
 	created:function(i,e){
-		$('#home').height($(document).height());
+		console.log(this.$el)
+	},
+	mounted:function(){
+		var that = this;
+		$(this.$el).scroll(function(){
+			var y = $(this).scrollTop();
+			$(this).children().children().each(function(){
+				if($(this).offset().top + $(this).css('margin-bottom').split('p')[0]<10){
+					$(that.$el).css({'backgroundImage':'url("images/'+($(this).index()+1)+'.jpg")'});
+				}
+			})
+		})
 	}
 })
