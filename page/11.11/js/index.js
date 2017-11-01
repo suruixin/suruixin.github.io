0d797cbe5aa38f4a37012886828a1b73690913ed
suruixin <507811581@qq.com>
2017-11-01 09:13:45 +0800

    3
    3

--- page/11.11/js/index.js
+++ page/11.11/js/index.js
@@ -8,3 +8,19 @@ var R = new roll({parents:'.activity-title-right',data:jsons,btn:true,classname:
 var jsons2 = [{name:'倍润清 王小5 填单 18000 2017-10-30'},{name:'倍润清 王小6 填单 18000 2017-10-30'},{name:'倍润清 王小1 填单 18000 2017-10-30'},{name:'倍润清 王小2 填单 18000 2017-10-30'},{name:'倍润清 王小3 填单 18000 2017-10-30'},{name:'倍润清 王小4 填单 18000 2017-10-30'}]
 var X = new roll({parents:'.activity-shape-right',data:jsons2,btn:false,classname:'activity-shape-content',animatetime:15000,settime:15000});
 X.engine();
+
+
+//倒计时
+var ts = new Date('2017-11-7 00:00:00').getTime();
+$('.activity-content-date').countdown({
+	timestamp	: ts
+});
+
+
+//完成度
+achieve({parent:'.activity-content-aim',keys:[{name:'总目标业绩',contrase:0},{name:'冲刺目标业绩',contrase:0},{name:'已完成目标业绩',contrase:1},{name:'应完成业绩目标',contrase:2}]})
+
+
+//详细数据
+var datas = [{num:1,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'0'},{num:2,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'0'},{num:3,name:'ccc',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:4,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:5,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:6,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:7,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:8,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:9,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:10,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:11,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:12,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:13,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:14,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'},{num:15,name:'倍润清',num1:50000,num2:80000,num3:8000,num4:'100%'}]
+table({data:datas,parent:'.activity-content-table',head:'业绩排行榜',titlename:['排行榜','项目组','总目标','冲刺目标','已完成业绩','完成率']})
--- page/11.11/js/libs/build.js
+++ page/11.11/js/libs/build.js
@@ -1,5 +1,29 @@
 console.log($.fn.jquery)
+//数字转货币
+function formatCurrency(num) {
+    num = num.toString().replace(/\$|\,/g,'');
+    if(isNaN(num))
+        num = "0";
+    sign = (num == (num = Math.abs(num)));
+    num = Math.floor(num*100+0.50000000001);
+    cents = num%100;
+    num = Math.floor(num/100).toString();
+    if(cents<10)
+    cents = "0" + cents;
+    for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
+    num = num.substring(0,num.length-(4*i+3))+','+
+    num.substring(num.length-(4*i+3));
+    return (((sign)?'':'-') + num + '.' + cents);
+}
+//数字转货币end
 
+
+
+var num_all = 0,//总目标
+	num_spurt = 0,//冲刺目标
+	num_acc = 0,//完成值
+	arr_ground = [],//项目组\姓名
+	arr_rate = [];//完成率
 var roll = function(roll){
 	this.data = roll.data;
 	this.classname = roll.classname||'';
@@ -67,6 +91,182 @@ roll.prototype = {
 }
 
 
+//倒计时
+var days	= 24*60*60,
+	hours	= 60*60,
+	minutes	= 60;
+$.fn.countdown = function(prop){
+	var options = $.extend({
+		callback	: function(){},
+		timestamp	: 0
+	},prop);
+	
+	console.log(options)
+	var left, d, h, m, s, positions;
+	init(this, options);
+	positions = this.find('.position');
+	(function tick(){
+		left = Math.floor((options.timestamp - (new Date())) / 1000);
+		
+		if(left < 0){
+			left = 0;
+		}
+		d = Math.floor(left / days);
+		updateDuo(0, 1, d);
+		left -= d*days;
+		h = Math.floor(left / hours);
+		updateDuo(2, 3, h);
+		left -= h*hours;
+		m = Math.floor(left / minutes);
+		updateDuo(4, 5, m);
+		left -= m*minutes;
+		s = left;
+		updateDuo(6, 7, s);
+		options.callback(d, h, m, s);
+		setTimeout(tick, 1000);
+	})();
+	
+	function updateDuo(minor,major,value){
+		switchDigit(positions.eq(minor),Math.floor(value/10)%10);
+		switchDigit(positions.eq(major),value%10);
+	}
+	
+	return this;
+};
+function init(elem, options){
+	console.log(elem)
+	elem.addClass('countdownHolder');
+	$.each(['Days','Hours','Minutes','Seconds'],function(i,_e){
+		var times = "";
+		switch(_e){
+			case 'Days':
+				times = '天';
+			break;
+			case 'Hours':
+				times = '时';
+			break;
+			case 'Minutes':
+				times = '分';
+			break;
+			case 'Seconds':
+				times = '秒';
+			break;
+		}
+		$('<span class="count'+this+' count">').html(
+			'<span class="position">'+
+				'<span class="digit static">0</span>'+
+			'</span>'+
+			'<span class="position">'+
+				'<span class="digit static">0</span>'+
+			'</span>'+
+			'<span class="counttime">'+times+'</span>'
+		).appendTo(elem);
+		
+		
+	});
+}
+function switchDigit(position,number){
+	
+	var digit = position.find('.digit')
+	
+	if(digit.is(':animated')){
+		return false;
+	}
+	
+	if(position.data('digit') == number){
+		return false;
+	}
+	
+	position.data('digit', number);
+	
+	var replacement = $('<span>',{
+		'class':'digit',
+		css:{
+			top:'-2.1em',
+			opacity:0
+		},
+		html:number
+	});
+	digit.before(replacement).removeClass('static').animate({top:'50px',opacity:0},'fast',function(){
+			digit.remove();
+		})
+
+	replacement.delay(100).animate({top:0,opacity:1},'fast',function(){
+			replacement.addClass('static');
+		});
+}
+
+
+//完成度
+function achieve(ach){
+	var _a = $('<div>').addClass('achieve-content').appendTo(ach.parent);
+	var _b = $('<ul>').addClass('achieve-data').appendTo(ach.parent);
+	achieve_data({'keys':ach.keys,parent:_b});
+	achieve_update(_a,_b)
+}
+function achieve_data(obj){
+	var html = '';
+	$.each(obj.keys,function(i,e){
+		html += '<li  data-contrase = "'+e.contrase+'"><span>'+e.name+'</span><span class="achieve-num">500000</span></li>'
+	})
+	obj.parent.html(html);
+}
+function achieve_update(_a,_b){
+	var bl = (_b.children('[data-contrase=1]').find('.achieve-num').text()*1)/(_b.children('[data-contrase=2]').find('.achieve-num').text()*1);
+	_b.children().each(function(){
+		$(this).find('.achieve-num').text(formatCurrency($(this).find('.achieve-num').text()))
+	})
+	var bls = 250-bl*300< -100?-100:(250-bl*300)
+	_a.html(Math.floor(bl*100)+'%').animate({'background-position-y':bls},300);
+	console.log(_b.find('.achieve-num').countUp())
+}
+
+
+//战报
+$('.activity-content-right label span').text(formatCurrency($('.activity-content-right label span').text()));
+$('.activity-content-right label span').countUp();
+
+
+//详细数据
+$('.activity-content-table')
+function table(tab){
+	var html = '<div class="activity-table-title">'+tab.head+'</div><div class="activity-table-box"><div class="activity-table-head"><table><thead><tr>';
+	arr_ground = [];
+	$.each(tab.titlename,function(i,e){
+		html += '<th>'+e+'</th>'
+	})
+	html += '</tr></thead></table></div><div class="activity-table-body"><table><tbody>';
+	$.each(tab.data,function(i,e){
+		html += '<tr>'
+			$.each(e,function(n,x){
+				html += '<td>'+x+'</td>'
+			})
+		html += '</tr>'
+	})
+	html += '</tbody></table></div>'
+	$(tab.parent).html(html);
+	table_time()
+}
+
+function table_time(){
+	var set_time = setInterval(a,10000);
+	function a(){
+		$('.activity-table-body tr').each(function(){
+			if(!$(this).is(':hidden')){
+				if($(this).offset().top-$(this).parent().offset().top+$(this).outerHeight()<$(this).closest('.activity-table-body').height()){
+					$(this).animate({'margin-top':100,opacity:0},600,function(){
+						$(this).css({opacity:1}).hide();
+						if($(this).index()==$(this).siblings().length){
+							$('.activity-table-body tr').show();
+						}
+					})
+				}
+			}
+		})
+	}
+}
+
+
 
 	
 $('button').on('click',function(){
@@ -81,3 +281,13 @@ $('button').on('click',function(){
 		$(this).css({'position':'relative',transform:'scale(1)'}).insertBefore($('.title2 tr').eq(0))
 	})
 })
+
+
+
+//shade
+$('.shade-bottom').on('click',function(){
+	$('.shade').hide(600);
+})
+function shadeoff(){
+	setTimeout(function(){$('.shade-bottom').click()},5000)
+}
