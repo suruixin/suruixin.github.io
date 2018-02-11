9652a34004e9d2bb26bccf23c9a8f6d306615ff5
suruixin <507811581@qq.com>
2018-02-11 15:05:54 +0800

    END

--- node/all.js
+++ node/all.js
@@ -44,12 +44,37 @@ server.engine('html',consolidate.ejs);
 server.use('/', (req,res) => {
 	var urls = urllib.parse(req.url,true);
 	if(urls.pathname == '/user'){
-		var srx = 'srx';
-		console.log(`SELECT * FROM 'user_table' WHERE 'username' = '${srx}';`)
-		db.query("SELECT * FROM 'user_table' WHERE 'username' = 'srx';", (err,data) => {
-			console.log(err)
-			console.log(data)
-		});
+		var name = urls.query.name;
+		var pass = urls.query.pass;
+		if(urls.query.act == 'signin'){
+			if(name === ''){
+				res.send({code:2,msg:'不可为空'})
+			}else if(name.length < 6){
+				res.send({code:2,msg:'用户名不可小于6位'})
+			}else if(name.length > 18){
+				res.send({code:2,msg:'用户名不可大于18位'})
+			}else{
+				db.query(`SELECT * FROM \`user_table\` WHERE \`username\` = '${name}';`, (err,data) => {
+					if(err){
+						res.send({code:1,msg:'内部错误'})
+					}else{
+						if(data.length === 0){
+
+						}else{
+							res.send({code:2,msg:'用户名已存在'})
+						}
+					}	
+				})
+			}
+		}else if(urls.query.act == 'login'){
+
+		}
+		console.log(urls.query)
+		//var srx = ;
+		// db.query(`SELECT * FROM \`user_table\` WHERE \`username\` = '${srx}';`, (err,data) => {
+		// 	console.log(err)
+		// 	console.log(data)
+		// });
 	}
 	res.render('index.ejs',{})
 });
