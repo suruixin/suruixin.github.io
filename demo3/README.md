d7378bd9de453a4f9a2393b8665d552cf11ed800
suruixin <507811581@qq.com>
2018-04-23 17:53:40 +0800

    gitnone

--- demo3/README.md
+++ demo3/README.md
@@ -1,4 +1,4 @@
-# demo
+# demo3
 
 > A Vue.js project
 
---
+++ demo3/src/store/state.js
@@ -0,0 +1,11 @@
+const state = {
+	count: 0,
+	obj:{a:1,b:2},
+	todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false }
+    ]
+}
+
+
+export default state
