19e6ba45285a8bfdcc9e629eeb18661d78e9ed35
苏瑞鑫 <suruixin@tongzhi.com.cn>
2018-12-29 10:00:49 +0800

    清空

---
+++ soundCode/src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <router-link :to="'/axios'" v-for="(item,index) in axios" :key="index"></router-link>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "home",
+      data(){
+        return {
+          tab: ['axios']
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
