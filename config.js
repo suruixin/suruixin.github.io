f16259e34133cc5a4d6971cc30a860ac20855514
四月 <www.507811581@qq.com>
2019-02-22 22:29:20 +0800

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+window.config = {
+  server: 'https://api.github.com',
+  siteName: 'April',
+  navbar: [
+    {
+      label: '首页',
+      link: '/'
+    }, {
+      label: '博客',
+      link: '/blog'
+    }
+  ],
+  github: {
+    user: 'suruixin',
+    repo: 'blog'
+  }
+}
